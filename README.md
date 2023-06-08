956f2db944f3973ed1d1153c4b04ecbb50d526f8
dushyanth9652narravula <narravula.dushyanthsai@gmail.com>
2023-06-08 11:32:06 +0530

    Preprocessing Package for NLP

---
+++ README.md
@@ -0,0 +1,7 @@
+# Preprocessing Text Python Package
+
+
+#### Course Link : https://bit.ly/intro_nlp
+
+
+This  Python package is prepared by N.Dushyanth Sai Chowdary. 
---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.md','r') as file:
+
+	long_desription = file.read()
+
+
+setuptools.setup(
+
+		name = 'preprocess_nlp', # This should be unique
+		version = '0.01',
+		author = 'N.Dushyanth Sai Chowdary',
+		author_email = 'narravula.dushyanthsai@gmail.com',
+		description = 'This is preprocessing package',
+		long_desription = long_desription,
+		long_description_content_type = 'text/markdown',
+		packages = setuptools.find_packages(),
+		classifiers = [
+		'Programming Language  :: Python :: 3',
+		'License :: OSI Aproved :: MIT License',
+		'Operating System :: OS Independent' 
+		],
+		python_requires = '>=3.0'
+	)
